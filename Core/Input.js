a1a4c69da0809e15d529821c8e6fd5c9bf637ea9
Kunal Singh <kunal1998doon@gmail.com>
2022-07-04 00:07:47 +0530

    Resolved issue with multiple event listeners  being attached

--- Core/Input.js
+++ Core/Input.js
@@ -3,30 +3,18 @@ class InputHandler {
     static rl = readline.createInterface(process.stdin, process.stdout);
     static demoSampler(prompt = 'Input>',type) {
         return new Promise(function (resolve, reject) {
-            InputHandler.rl.setPrompt(prompt)
-            InputHandler.rl.prompt();
-            InputHandler.rl.on('line', function (line) {
-                if (line === "exit" || line === "quit" || line == 'q') {
-                    InputHandler.rl.close()
-                    return // bail here, so rl.prompt() isn't called again
-                }
-                if(type === 1){
-                    const val = line.split(',');
-                    resolve(val);
-                    return;
-                }
-                if(type === 2){
-                    const inputMap = line.split(":");
+            if (type === 1) {
+                InputHandler.rl.question(prompt, (data) => {
+                    resolve(data.split(','));
+                });
+            }
+            if (type === 2) {
+                InputHandler.rl.question(prompt, (data) => {
+                    const inputMap = data.split(":");
                     resolve(inputMap);
-                    return;
-                }
-                InputHandler.rl.prompt()
-
-            }).on('close', function () {
-                console.log('bye');
-                resolve(1);
-            });
-        })
+                });
+            }
+        });
     }
 
     static initPlayerInput(playerTag){
